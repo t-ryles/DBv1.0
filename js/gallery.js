9ae4852db68e63e705f1750eabd30d1481895da1
t-ryles <tajhakeem.ryles@gmail.com>
2022-01-16 10:34:09 -0600

    Updated
    --Added
    More images to the Gallery page.
    Home image folder
    
    --Updated
    All the headers and home page image links
    
    --Need to do
    Build the contact page card.

--- js/gallery.js
+++ js/gallery.js
@@ -3,7 +3,42 @@ let gallery = document.getElementById('gallery');
 let popUp = document.getElementById('popUp');
 let selectedImage = document.getElementById('selectedImage');
 
-const imageInd = [ 1, 2, 3, 4, 5 ];
+const imageInd = [
+	1,
+	2,
+	3,
+	4,
+	5,
+	6,
+	7,
+	8,
+	9,
+	10,
+	12,
+	13,
+	14,
+	15,
+	16,
+	17,
+	18,
+	19,
+	20,
+	21,
+	22,
+	23,
+	24,
+	25,
+	26,
+	27,
+	28,
+	29,
+	30,
+	31,
+	32,
+	34,
+	35,
+	36
+];
 selectedImage = null;
 
 imageInd.forEach((i) => {
